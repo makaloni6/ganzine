d90dd407f0bc6bc68e1f2eb7ffa3898e0546d7ac
makaloni6 <ok@MacBook-Pro-Taisuke.local>
2024-06-15 20:56:44 +0900

    関数の追加

--- jancode.py
+++ jancode.py
@@ -1,16 +1,50 @@
 import csv
-from account import acc
+import pickle
+import os
+from dotenv import load_dotenv
 
-def setJancode(acc):
-    with open('./csv/{}.csv'.format(acc), 'r') as f:
+
+def getJancode() -> dict:
+    jancode = {}
+    with open('./.csv', 'r') as f:
         reader = csv.DictReader(f)
 
+    return jancode
 
-def makeCSV():
-    pass
+
+def getCodes(acc: str) -> dict:
+    with open('./pickle/code_dict_{}.pickle'.format(acc), 'rb') as f:
+        code_dict = pickle.load(f)
+    
+    return code_dict
+
+
+def makeCSV(acc: str, code: dict, jancode: dict):
+
+    header = ['code', 'jancode']
+    data = []
+    for a in code.items():
+        data.append([code[a], jancode[a]])
+
+    with open('./csv/{}.csv'.format(acc), 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(header)
+        writer.writerows(data)
+
 
 def ftpCSV():
     pass
 
+
 def main():
-    pass
+    load_dotenv()
+    jancode = getJancode()
+    accounts = os.environ['ACCOUNTS']
+    for acc in accounts:
+        code = getCodes(acc)
+        makeCSV(acc, code, jancode)
+
+
+if __name__ == '__main__':
+    
+    main()
